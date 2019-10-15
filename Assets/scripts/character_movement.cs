e683730d1e791d9f09f7e71bd1b900502e54ac30
darketomaly <darketomaly@gmail.com>
2019-10-15 08:38:33 -0500

    character movement

---
+++ Assets/scripts/character_movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class character_movement : MonoBehaviour {
+
+    public float speed;
+
+    public Camera character_view;
+
+    void Start() {
+
+        character_view = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        
+    }
+
+
+    void Update() {
+
+        //character movement
+        float x_axis_movement = Input.GetAxis("Horizontal") * speed;
+        x_axis_movement *= Time.deltaTime;
+        transform.Translate(x_axis_movement, 0, 0);
+
+        //camera rotation
+        float y_axis_view_rotation = Input.GetAxis("Mouse X") * 75;
+        y_axis_view_rotation *= Time.deltaTime;
+        character_view.transform.Rotate(0, y_axis_view_rotation, 0);
+        
+    }
+}
