a623528c41473bcda4a1d506172f437f26afc739
darketomaly <darketomaly@gmail.com>
2019-10-15 20:49:54 -0500

    Safespots

--- Assets/scripts/character_movement.cs
+++ Assets/scripts/character_movement.cs
@@ -2,28 +2,34 @@
 
 public class character_movement : MonoBehaviour {
 
-    public float speed;
-
-    public Camera character_view;
+    private Camera character_view;
 
     void Start() {
 
-        character_view = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        character_view = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>(); 
         
     }
-
 
     void Update() {
 
-        //character movement
-        float x_axis_movement = Input.GetAxis("Horizontal") * speed;
-        x_axis_movement *= Time.deltaTime;
-        transform.Translate(x_axis_movement, 0, 0);
-
-        //camera rotation
-        float y_axis_view_rotation = Input.GetAxis("Mouse X") * 75;
-        y_axis_view_rotation *= Time.deltaTime;
-        character_view.transform.Rotate(0, y_axis_view_rotation, 0);
+        MovementAndRotation();
         
     }
+
+    private void MovementAndRotation() {    
+        
+        //character movement
+        float x_axis_movement = Input.GetAxis("Horizontal") * 10;
+        x_axis_movement *= Time.deltaTime;
+        transform.Translate(x_axis_movement, 0, 0, Space.World);
+
+        //character and camera rotation
+        float y_axis_view_rotation = Input.GetAxis("Mouse X") * 75;
+        y_axis_view_rotation *= Time.deltaTime;
+        character_view.transform.Rotate(0, y_axis_view_rotation, 0, Space.World);
+        transform.Rotate(0, y_axis_view_rotation, 0 /*, Space.World*/); //quitar si se quiere hacer como el juego og.
+
+    }
+
 }
