df83833883683613a28fb223f4e4894e06e29379
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2019-10-16 22:38:30 -0500

    La nave del Olvido
    Ya con musica y efectos

--- Assets/chino_scripts/Menu.cs
+++ Assets/chino_scripts/Menu.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    Animator myyanim;
+    public Animator myyanim;
 
     [SerializeField]
     private Transform CameraTrans;
 
-   
+    public AudioSource misource;
+
+
+    public List<AudioClip> MisClips = new List<AudioClip>();
     public List<GameObject> positions = new List<GameObject>(); //Posiciones de mierda
 
     int indice = 0; //indice de la posicion de mierda
-    bool reachedpos = false; //ya llegue a la pos de mierda?
+    bool reachedpos = false, reachedSpace = false; //ya llegue a la pos de mierda?
 
     [Range(0.1f, 5.0f)]
     public float tiempMovim; //En cuanto time quiero que la camara de mierda se mueva de A a B amiguito
@@ -31,6 +34,17 @@ public class Menu : MonoBehaviour
         {
             MoveCamToPos();
         }
+
+        if (!reachedSpace)
+        {
+            MoveCamToSpaceShip();
+        }
+    }
+
+    public void ChangeClip(int _indice)
+    {
+        misource.clip = MisClips[_indice];
+        misource.Play();
     }
 
     void MoveCamToPos()
@@ -50,7 +64,26 @@ public class Menu : MonoBehaviour
         {
             reachedpos = true;
         }
-       
+    }
+
+    void MoveCamToSpaceShip()
+    {
+        if (valorT < 1.0f) //si no es el 100%
+        {
+            valorT += factMovim * Time.deltaTime;//Avanzamos valor en T
+            if (valorT >= 1.0f)//Seguridad de no ir mas haya del B
+            {
+                valorT = 1.0f;
+            }
+            //Actualizamos posicion
+            CameraTrans.position = Vector3.Lerp(CameraTrans.position, positions[indice].transform.position, valorT);
+            CameraTrans.rotation = Quaternion.Lerp(CameraTrans.rotation, positions[indice].transform.rotation, valorT);
+        }
+        else
+        {
+            CameraTrans.position = positions[indice].transform.position; //sigue al transform ese
+            CameraTrans.rotation = positions[indice].transform.rotation;
+        }
     }
 
     public void ChangePosition(int _index)//cambiar posicion a la que ira la camara de mierda
@@ -58,7 +91,19 @@ public class Menu : MonoBehaviour
         reachedpos = false;
         valorT = 0.0f;
         indice = _index;
+
+        reachedSpace = true;
     }
+
+    public void FollowShip()
+    {
+        reachedpos = true;
+        reachedSpace = false;
+        indice = 2;
+        valorT = 0.0f;
+    }
+
+    
 
     public void QuitGame()//Para salir del puto juego de mierda
     {
@@ -68,5 +113,10 @@ public class Menu : MonoBehaviour
     public void ToggleGameObject(GameObject mierda)//Le permite al boton activar/desactivar cualquier mierda
     {
         mierda.SetActive(!mierda.activeSelf);
+    }
+
+    public void pasarDeEscena()
+    {
+        myyanim.SetBool("FadeOut", true);
     }
 }
