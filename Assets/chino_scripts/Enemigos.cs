93b9f92fd2afefc7ea488638763df828deadadf8
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2019-10-19 18:26:25 -0500

    SIU

---
+++ Assets/chino_scripts/Enemigos.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigos : MonoBehaviour
+{
+    public Transform Player;
+
+    public bool DasherGuy = false;
+    public float bottomLimit = -5f;
+
+    public float velocidaddemovimiento = 3f, rango = 10.0f, velocidadhaciabajo = 0.0f, tiempodedisparo = 1.0f;
+
+    bool right = true;
+
+    Rigidbody rbody;
+    Vector2 direccion, posactual;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        posactual = new Vector2(transform.position.x, transform.position.y);
+
+        if (DasherGuy)
+        {
+            if(Player.position.x >= transform.position.x)
+            {
+                right = false;
+            }
+            else
+            {
+                right = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        posactual = new Vector2(transform.position.x, transform.position.y);
+
+        if (DasherGuy)
+        {
+
+        }
+        else
+        {
+            MovimientoNormal();
+        }
+    }
+
+    public void MovimientoNormal()
+    {
+        if (posactual.y > bottomLimit)
+        {
+            direccion = new Vector2(Mathf.Sin(Time.time * rango) * velocidaddemovimiento, -velocidadhaciabajo);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+             
+        rbody.MovePosition(posactual + direccion * Time.deltaTime);
+    }
+
+    public void MovimientoDasher()
+    {
+        if(right)
+    }
+
+    IEnumerator WaitToMove(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+    }
+}
---
+++ Assets/chino_scripts/ObjectPooler.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string etiqueta;
+        public GameObject prefab;
+        public int tamanio;
+    }
+
+    #region Singleton
+    //==============Singleton=========
+
+    public static ObjectPooler instancia;
+
+    private void Awake()
+    {
+        instancia = this;
+    }
+
+    //=================================
+    #endregion
+
+    public List<Pool> ConjuntoPools;
+    public Dictionary<string, Queue<GameObject>> poolDiccionario;
+
+    //Al principio
+    void Start()
+    {
+        //Hace nuevo apartado en el diccionario
+        poolDiccionario = new Dictionary<string, Queue<GameObject>>();
+
+        //para cada pool en la lista de pools
+        foreach (Pool pool in ConjuntoPools)
+        {
+            //crea un Queue (fila)
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            //itera n veces y spawnea y desactiva los objetos
+            for (int i = 0; i < pool.tamanio; i++)
+            {
+                GameObject objetillo = Instantiate(pool.prefab);
+                objetillo.SetActive(false);
+                objectPool.Enqueue(objetillo);
+            }
+            //Añade este pool al diccionario
+            poolDiccionario.Add(pool.etiqueta, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 posicion, Quaternion rotacion)
+    {
+        //Pa que no existan errores por pasar tag invalido
+        if (!poolDiccionario.ContainsKey(tag))
+        {
+            Debug.LogWarning("El Pool con tag " + tag + " no existe");
+            return null;
+        }
+        //=========================================================\\
+
+        GameObject ObjetoaSpawnear = poolDiccionario[tag].Dequeue();
+
+        ObjetoaSpawnear.SetActive(true);
+        ObjetoaSpawnear.transform.position = posicion;
+        ObjetoaSpawnear.transform.rotation = rotacion;
+        if (tag == "Particulas")
+        {
+            ObjetoaSpawnear.GetComponent<ParticleSystem>().Play();
+        }
+        if (tag == "Explosiones")
+        {
+            ObjetoaSpawnear.GetComponent<ParticleSystem>().Play();
+        }
+        poolDiccionario[tag].Enqueue(ObjetoaSpawnear);
+
+        return ObjetoaSpawnear;
+    }
+
+    public GameObject SpawnBulletFromPool(string tagbullet, Vector3 posicion, Quaternion rotacion, Vector2 direccion)
+    {
+        //Pa que no existan errores por pasar tag invalido
+        if (!poolDiccionario.ContainsKey(tagbullet))
+        {
+            Debug.LogWarning("El Pool con tag " + tagbullet + " no existe");
+            return null;
+        }
+
+        GameObject ObjetoaSpawnear = poolDiccionario[tagbullet].Dequeue();
+
+        ObjetoaSpawnear.SetActive(true);
+        ObjetoaSpawnear.transform.position = posicion;
+        ObjetoaSpawnear.transform.rotation = rotacion;
+
+        Bullet mibull = ObjetoaSpawnear.GetComponent<Bullet>();
+      /*  mibull.DireccDisparo = direccion;
+        mibull.activated = true;
+
+
+        poolDiccionario[tagbullet].Enqueue(ObjetoaSpawnear);
+        mibull.activar();*/
+        return ObjetoaSpawnear;
+    }
+}
+
