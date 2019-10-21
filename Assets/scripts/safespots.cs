c84dd34c429c2fa7c80a0a7a1e4a3330f531dca8
SorenGedeon <32682025+TerrierBrakk@users.noreply.github.com>
2019-10-21 16:55:13 -0500

    update safesposts.cs

--- Assets/scripts/safespots.cs
+++ Assets/scripts/safespots.cs
@@ -2,27 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class safespots : MonoBehaviour {
+public class safespots : MonoBehaviour
+{
 
     public GameObject[] _safespots;
 
-    void Start() {
+    void Start()
+    {
 
-        InvokeRepeating("PlaceSafespot", 5.0f, Random.Range(5.0f, 15.0f));
-        
+
+
     }
 
-    private void PlaceSafespot() {
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            PlaceSafespot();
+
+        }
+    }
+
+    private void PlaceSafespot()
+    {
 
         int index = Random.Range(0, _safespots.Length);
+        int counter = 0;
 
-        if (_safespots[index].activeSelf) {
+        if (_safespots[index].activeSelf) return; //si ya se encuentra activo
 
-            return;
+        _safespots[index].transform.position = new Vector3(Random.Range(-40.0f, 40.0f), 0, 5);
+
+        for (int i = 0; i < _safespots.Length; i++)
+        {
+
+            if (index != i)
+            {
+
+                //si se encuentra muy cerca de  un safespot activo
+                if (_safespots[i].activeSelf)
+                {
+
+                    if (Vector3.Distance(_safespots[index].GetComponent<Collider>().bounds.center, _safespots[i].transform.GetComponent<Collider>().bounds.center) < 10.0f)
+                    {
+
+                        counter++;
+
+                    }
+                }
+
+            }
+
         }
 
-        _safespots[index].GetComponent<safespot_health>().renew();
-        _safespots[index].SetActive(true);
+        if (counter < 1)
+        { //si no se encuentra muy cerca de un safespot activo
+
+            _safespots[index].GetComponent<safespot_health>().renew();
+            _safespots[index].SetActive(true);
+
+        }
 
     }
+
 }
